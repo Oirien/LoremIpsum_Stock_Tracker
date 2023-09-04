8b26245a86d3b9e152576a8378605260582766f2
Oirien <robert.mayo.ch@gmail.com>
2023-09-04 17:37:57 +0100

    added basic api fetching to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,50 @@
-import { useState } from "react";
+import React from 'react';
+import axios from 'axios';
+import { useQuery } from 'react-query';
+
+const fetchDBData = async () => {
+    const res = await axios.get('http://localhost:9000/api/users/');
+    return res.data;
+};
+
+const fetchAPIData = async () => {
+    const res = await axios.get(
+        'https://api.twelvedata.com/stocks?exchange=LSE',
+    );
+    return res.data;
+};
 
 function App() {
-	return <></>;
+    const {
+        data: userData,
+        error: userError,
+        isLoading: userLoading,
+    } = useQuery('users', fetchDBData);
+    const {
+        data: stockData,
+        error: stockError,
+        isLoading: stockLoading,
+    } = useQuery('stocks', fetchAPIData);
+
+    if (userLoading || stockLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (userError) {
+        console.error('Database Error:', userError);
+        return <div>Error fetching user data. Please try again later.</div>;
+    }
+
+    if (stockError) {
+        console.error('API Error:', stockError);
+        return <div>Error fetching stock data. Please try again later.</div>;
+    }
+
+    if (!stockData || stockData.length === 0) {
+        return <div>No stock data available.</div>;
+    }
+
+    return <>{stockData.data[0].name}</>;
 }
 
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {},
+        mutations: {},
+    },
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
+    </React.StrictMode>,
 );
