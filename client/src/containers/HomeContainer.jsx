e77e544947be979a23d9430790cabdeb7b120cf1
George Tegos <george.tegos.22@gmail.com>
2023-09-05 12:41:24 +0100

    HomeCointainer first stage

--- client/src/containers/HomeContainer.jsx
+++ client/src/containers/HomeContainer.jsx
@@ -1,27 +1,81 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useState } from 'react';
 
 const HomeWrapper = styled.div`
-    background-color: red;
+    /* background-color: red; */
+    border:2px solid yellow;
+    gap: 8rem;
+    display: flex;
+    align-items: center;
+`;
+
+const FilterArea = styled.div`
+    min-width: 6rem;
+    display: flex;
+    flex-flow: column wrap;
+    margin-left: 20px;
+    gap: 10px;
+    /* border: 2px solid red; */
+`;
+
+const FilterAreaLi = styled.li`
+    text-align: center;
+    margin: 1rem;
+    border: 1px solid white;
+    border-radius:30%;
+`;
+
+const StocksListUl = styled.ul`
+ /* border:1px solid yellow; */
+ display: flex;
+ flex-flow: wrap;
+ margin-left: -100px;
+`;
+
+const StocksListLi = styled.li`
+    min-width: 25%;
+    border:1px solid pink;
+    margin: 30px 20px;
 `;
 
 
 
+const HiddenComponent = styled.div`
+    border:1px solid green;
+    height: 200px;
+`;
+
 function HomeContainer() {
+
+    const [isShown, setIsShown] = useState(false);
+    const items = [1,2,3,4,5,6,7,8,9,10]
+
+   
+    console.log(isShown)
   return (
     <>
     <HomeWrapper>
-        <div>
-            Filter Area
-        </div>
+        <FilterArea>
+            <FilterAreaLi> 1 - 9</FilterAreaLi>
+            <FilterAreaLi> 10 - 19</FilterAreaLi>
+            <FilterAreaLi> 20 - 29</FilterAreaLi>
+        </FilterArea>
 
-        <div>
-            Stocks List
-        </div>
+        <StocksListUl>
+            {items.map((item,index) => 
+            <StocksListLi key={index} 
+            onMouseEnter={() => setIsShown(true)}
+            onMouseLeave={() => setIsShown(false)}>
+                {item}
+            </StocksListLi>)}
+        </StocksListUl>
 
-        <div>
+        {isShown && 
+        <HiddenComponent>
             Hidden
-        </div>
+        </HiddenComponent> 
+        }
     
     </HomeWrapper>
     
