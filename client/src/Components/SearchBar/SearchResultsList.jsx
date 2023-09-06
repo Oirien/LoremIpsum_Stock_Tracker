f12fc5524641678d8cf6e77e5af6a63fb362837d
Oirien <robert.mayo.ch@gmail.com>
2023-09-06 22:01:51 +0100

    stock seed files changed, will need to reload them into mongodb

--- client/src/Components/SearchBar/SearchResultsList.jsx
+++ client/src/Components/SearchBar/SearchResultsList.jsx
@@ -2,32 +2,30 @@ import React, { useEffect, useRef } from 'react';
 import SearchResult from './SearchResult';
 
 const SearchResultsList = ({ searchBar, setSearchBar }) => {
+    useEffect(() => {
+        document.addEventListener('click', handleEmptySearchResults, true);
+    }, []);
 
-    useEffect(()=>{
-        document.addEventListener("click",handleEmptySearchResults,true)
-    },[])
+    const refOne = useRef(null);
 
-    const refOne = useRef(null)
-
-    const handleEmptySearchResults = (e) =>{
-        if(!refOne.current.contains(e.target)){
-            if(searchBar.length <= 0){
-                console.log("Clicked out")
-            }else{
-                setSearchBar([])
+    const handleEmptySearchResults = (e) => {
+        if (!refOne.current.contains(e.target)) {
+            if (searchBar.length <= 0) {
+                console.log('Clicked out');
+            } else {
+                setSearchBar([]);
             }
-            
-        }else{
-            console.log('clicked in')
+        } else {
+            console.log('clicked in');
         }
-    }
+    };
 
     return (
         <div className="search-results__list" ref={refOne}>
             {searchBar.map((search, i) => {
                 return (
                     <SearchResult
-                        searchResult={search.name}
+                        searchResult={search.description}
                         searchId={search.symbol}
                         key={i}
                     />
