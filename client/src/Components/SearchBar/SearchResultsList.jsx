c953a4c1f8f0cb0b814e39e562324149056b6040
George Tegos <george.tegos.22@gmail.com>
2023-09-06 09:50:12 +0100

    portfolio onhover show

--- client/src/Components/SearchBar/SearchResultsList.jsx
+++ client/src/Components/SearchBar/SearchResultsList.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import SearchResult from './SearchResult';
 
-const SearchResultsList = ({ searchBar }) => {
+const SearchResultsList = ({ searchBar, setSearchBar }) => {
+
+    useEffect(()=>{
+        document.addEventListener("click",handleEmptySearchResults,true)
+    },[])
+
+    const refOne = useRef(null)
+
+    const handleEmptySearchResults = (e) =>{
+        if(!refOne.current.contains(e.target)){
+            if(searchBar.length <= 0){
+                console.log("Clicked out")
+            }else{
+                setSearchBar([])
+            }
+            
+        }else{
+            console.log('clicked in')
+        }
+    }
+
     return (
-        <div className="search-results__list">
+        <div className="search-results__list" ref={refOne}>
             {searchBar.map((search, i) => {
                 return (
                     <SearchResult
