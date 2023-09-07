44537612b8660f2197cfa73a71e0a40766a0852b
John <ashton.basile@gmail.com>
2023-09-07 11:20:20 +0100

    the user details for the first user in the database now appear on the account page

--- client/src/containers/AccountContainer.jsx
+++ client/src/containers/AccountContainer.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { useOutletContext } from 'react-router-dom';
 
 function AccountContainer() {
-    return <div>AccountContainer</div>;
+    const { userData } = useOutletContext();
+    const userOne = userData[0];
+    return (
+        <>
+            <div>AccountContainer</div>
+            <ul>
+                <li>Username: {userOne.username}</li>
+                <li>Email: {userOne.email}</li>
+                <li>Billing Address: {userOne.billing_address}</li>
+                <li>Phone: {userOne.phone}</li>
+            </ul>
+        </>
+    );
 }
 
 export default AccountContainer;
