e032167ac96e7fcde95bc8a221b33a7435fd890b
George Tegos <george.tegos.22@gmail.com>
2023-09-08 10:42:44 +0100

    split component , cant buy message

--- client/src/Components/Stock.jsx
+++ client/src/Components/Stock.jsx
@@ -16,8 +16,6 @@ import {
 import StockBuySell from './StockBuySell';
 
 function Stock() {
-    const [stocksBought, setStocksBought] = useState('');
-    const [stocksSold, setStocksSold] = useState('');
     const { symbol } = useParams();
     const { userData } = useOutletContext();
     const queryClient = useQueryClient();
@@ -65,155 +63,6 @@ function Stock() {
         return <div>Error fetching API data. Please try again later.</div>;
     }
 
-    const handleBuyStocks = (e) => {
-        e.preventDefault();
-
-        const stockSymbol = stockIndividualData.symbol;
-
-        const previousStocksAmount = Number(
-            userData[0].stocks
-                .filter((stock) => stock.symbol === stockIndividualData.symbol)
-                .map((stocks_number) => stocks_number.number_of_stocks_owned),
-        );
-        const newAmountOfStocks = stocksBought + previousStocksAmount;
-
-        const newAmountSpentForStock = Math.round(
-            stocksBought * stockIndividualData.close +
-                Number(
-                    userData[0].stocks
-                        .filter(
-                            (stock) =>
-                                stock.symbol === stockIndividualData.symbol,
-                        )
-                        .map((amount) => amount.amount_spent),
-                ),
-        );
-
-        const newStockData = {
-            symbol: stockSymbol,
-            number_of_stocks_owned: newAmountOfStocks,
-            amount_spent: newAmountSpentForStock,
-        };
-
-        const newStocks = [
-            ...userData[0].stocks.filter(
-                (stock) => stock.symbol !== stockSymbol,
-            ),
-            newStockData,
-        ];
-
-        const newLifeTimeSpend = Math.round(
-            stocksBought * stockIndividualData.close +
-                userData[0].lifetime_spend,
-        );
-
-        const newWallet = Math.round(
-            userData[0].wallet - stocksBought * stockIndividualData.close,
-        );
-
-        const config = {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                stocks: newStocks,
-                lifetime_spend: newLifeTimeSpend,
-                wallet: newWallet,
-            }),
-        };
-
-        console.log(newStocks);
-        fetch(`http://localhost:9000/api/users/${userData[0]._id}`, config)
-            .then((res) => res.json())
-            .then((data) => queryClient.invalidateQueries('users'));
-
-        setStocksBought('');
-    };
-
-    const handleSellStocks = (e) => {
-        e.preventDefault();
-
-        const stockSymbol = stockIndividualData.symbol;
-
-        const previousStocksAmount = Number(
-            userData[0].stocks
-                .filter((stock) => stock.symbol === stockIndividualData.symbol)
-                .map((stocks_number) => stocks_number.number_of_stocks_owned),
-        );
-        const newAmountOfStocks = previousStocksAmount - stocksSold;
-
-        const newAmountSpentForStock = Math.round(
-            Number(
-                userData[0].stocks
-                    .filter(
-                        (stock) => stock.symbol === stockIndividualData.symbol,
-                    )
-                    .map((amount) => amount.amount_spent),
-            ) -
-                stocksSold * stockIndividualData.close,
-        );
-
-        const newLifeTimeSpend = Math.round(
-            Number(
-                userData[0].lifetime_spend -
-                    stocksSold * stockIndividualData.close,
-            ),
-        );
-
-        const newWallet = Math.round(
-            Number(userData[0].wallet + stocksSold * stockIndividualData.close),
-        );
-
-        if (newAmountOfStocks > 0) {
-            const newStockData = {
-                symbol: stockSymbol,
-                number_of_stocks_owned: newAmountOfStocks,
-                amount_spent: newAmountSpentForStock,
-            };
-
-            const newStocks = [
-                ...userData[0].stocks.filter(
-                    (stock) => stock.symbol !== stockSymbol,
-                ),
-                newStockData,
-            ];
-
-            const config = {
-                method: 'PUT',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({
-                    stocks: newStocks,
-                    lifetime_spend: newLifeTimeSpend,
-                    wallet: newWallet,
-                }),
-            };
-            fetch(`http://localhost:9000/api/users/${userData[0]._id}`, config)
-                .then((res) => res.json())
-                .then((data) => queryClient.invalidateQueries('users'));
-        } else {
-            const newStocks = [
-                ...userData[0].stocks.filter(
-                    (stock) => stock.symbol !== stockSymbol,
-                ),
-            ];
-            const config = {
-                method: 'PUT',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({
-                    stocks: newStocks,
-                    lifetime_spend: newLifeTimeSpend,
-                    wallet: newWallet,
-                }),
-            };
-            fetch(`http://localhost:9000/api/users/${userData[0]._id}`, config)
-                .then((res) => res.json())
-                .then((data) => queryClient.invalidateQueries('users'));
-        }
-
-        console.log(newAmountSpentForStock);
-
-        setStocksSold('');
-    };
-
     return (
         <>
             <StocksWrapper>
@@ -251,14 +100,9 @@ function Stock() {
 
             <StockBuySell
                 stockIndividualData={stockIndividualData}
-                handleBuyStocks={handleBuyStocks}
-                handleSellStocks={handleSellStocks}
-                stocksBought={stocksBought}
-                stocksSold={stocksSold}
-                setStocksBought={setStocksBought}
-                setStocksSold={setStocksSold}
                 userData={userData}
-            />
+                queryClient={queryClient}
+            ></StockBuySell>
         </>
     );
 }
